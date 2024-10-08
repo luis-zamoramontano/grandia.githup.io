918b1c1582ca9aa556d8a888a07999d969b2169a
luis-zamoramontano <luismiguelmontanozamora@gmail.com>
2024-10-07 21:07:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const btnOpenElement = document.querySelector('#open')
+const btnCloseElement = document.querySelector('#close')
+
+btnCloseElement.disabled = true
+
+
+
+
+btnOpenElement.addEventListener('click',()=> {
+    btnOpenElement.disabled = true
+    btnCloseElement.disabled = false
+    const coverElement = document.querySelector('.cover')
+    coverElement.classList.add('open-cover')
+
+    setTimeout(()=>{
+    coverElement.style.zIndex = -1
+
+    const paperElement = document.querySelector('.paper')  
+    paperElement.classList.remove('close-paper')
+    paperElement.classList.add('open-paper')
+
+    }, 500)
+    
+    
+})
+
+btnCloseElement.addEventListener('click',()=> {
+    btnOpenElement.disabled = false
+    btnCloseElement.disabled = true
+
+    const coverElement = document.querySelector('.cover')
+    const paperElement = document.querySelector('.paper')    
+    paperElement.classList.remove('open-paper')
+    paperElement.classList.add('close-paper')
+
+
+    setTimeout(()=>{
+    coverElement.style.zIndex = 0
+    coverElement.classList.remove('open-cover')
+    
+    },500)
+    
+})
+
